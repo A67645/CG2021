ec30ef6e884231847a9482feff6c83100113b9b3
melpereira7 <melanipereira98@gmail.com>
2021-03-13 15:47:36 +0000

    tudo a funcionar, falta alterar o engine para guardar pontos em memotia

---
+++ parte1/cmake-build-release/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/Engine/engine.cpp.obj"
+  "CMakeFiles/engine.dir/Engine/tinyxml2.cpp.obj"
+  "engine.exe"
+  "engine.exe.manifest"
+  "engine.pdb"
+  "libengine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
